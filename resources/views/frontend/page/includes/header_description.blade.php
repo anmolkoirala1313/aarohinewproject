a2a2ff7ffa9610eacd7ebd9758c707eff88ccefb
anmolko <anmol@canosoft.com.np>
2024-06-11 23:54:35 +0545

    header description dynamic page

--- resources/views/frontend/page/includes/header_description.blade.php
+++ resources/views/frontend/page/includes/header_description.blade.php
@@ -1,17 +1,20 @@
-<div class="content-detail-block pb-100">
+<section class="blog-page section-space">
     <div class="container">
-        <div class="row row-gap-32">
-            <div class="col-12 col-xl-12">
-                <div class="content-para pr-55">
-                    <div class="text text-center">
-                        <div class="heading3 mt-30 mb-3">{{ $element->first()->subtitle ?? '' }}</div>
+        <div class="row gutter-y-60">
+            <div class="col-lg-12">
+                <div class="blog-details">
+                    <div class="sec-title sec-title--border sec-title--center">
+                        <h3 class="sec-title__title">{{ $element->first()->subtitle ?? '' }}</h3>
                     </div>
-                    <div class="body3 text-secondary text-align-justify custom-description">
-                        {!! $element->first()->description ?? '' !!}
+                    <div class="blog-card wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                        <div class="blog-card__content">
+                            <div class="blog-card__text blog-card__text--two text-align-justify custom-description">
+                                {!! $element->first()->description ?? '' !!}
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-
+            </div><
         </div>
     </div>
-</div>
+</section>
